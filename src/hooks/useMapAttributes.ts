e2039dbd972bfc69f8bc2daadb8066bd2e199245
kennethlili <lichikin1997@gmail.com>
2025-04-30 10:44:21 -0400

    feat: add debound on map move

--- src/hooks/useMapAttributes.ts
+++ src/hooks/useMapAttributes.ts
@@ -1,41 +1,48 @@
-import { MAP_ZOOM } from "@/constants/mapConfig";
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useMap } from "react-leaflet";
+import { useDebouncedCallback } from "use-debounce";
+import { MAP_ZOOM } from "@/constants/mapConfig";
+
+type MapBounds = {
+  west: number;
+  south: number;
+  east: number;
+  north: number;
+};
 
 export function useMapAttributes() {
   const map = useMap();
-  const [zoom, setZoom] = useState(MAP_ZOOM);
-  const [bounds, setBounds] = useState({
+  const [zoom, setZoom] = useState<number>(MAP_ZOOM);
+  const [bounds, setBounds] = useState<MapBounds>({
     west: 0,
     south: 0,
     east: 0,
     north: 0,
   });
 
-  useEffect(() => {
-    const updateAttributes = () => {
-      const mapBounds = map.getBounds();
-      setBounds({
-        west: mapBounds.getWest(),
-        south: mapBounds.getSouth(),
-        east: mapBounds.getEast(),
-        north: mapBounds.getNorth(),
-      });
-      const currZoom = map.getZoom();
-      setZoom(currZoom);
-    };
+  const handleMapMove = useDebouncedCallback(() => {
+    const bounds = map.getBounds();
+    setBounds({
+      west: bounds.getWest(),
+      south: bounds.getSouth(),
+      east: bounds.getEast(),
+      north: bounds.getNorth(),
+    });
+    setZoom(map.getZoom());
+  }, 500);
 
-    updateAttributes();
+  useEffect(() => {
+    handleMapMove();
 
     // Update attributes on move/zoom
-    map.on("moveend", updateAttributes);
-    map.on("zoomend", updateAttributes);
+    map.on("moveend", handleMapMove);
+    map.on("zoomend", handleMapMove);
 
     return () => {
-      map.off("moveend", updateAttributes);
-      map.off("zoomend", updateAttributes);
+      map.off("moveend", handleMapMove);
+      map.off("zoomend", handleMapMove);
     };
-  }, [map]);
+  }, [map, handleMapMove]);
 
   return { zoom, bounds };
 }
